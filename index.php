3c857c1511aed63a230ff770d22e81c1abd2a81e
thefrostandfire <72944729+thefrostandfire@users.noreply.github.com>
2021-01-16 19:23:59 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="FR">
+<head>
+	<title> Accueil </title>
+	<link rel="stylesheet" type="text/css" href="index.css">
+	<link rel="icon" type="image/png" sizes="16x16" href="..\img\pack.png">
+	<meta charset="utf-8">
+</head>
+<body>
+
+<?php 
+	include '../Menu/menu.php';
+?>
+
+</body>
+</html>
